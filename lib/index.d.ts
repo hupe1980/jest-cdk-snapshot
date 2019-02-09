52448178ac29e2a75267f15bb40b2a13314ab443
hupe1980 <hupe@Franks-MBP-2.fritz.box>
2019-02-09 18:36:50 +0100

    Initial commit

--- lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="jest" />
-declare global {
-    namespace jest {
-        interface Matchers<R> {
-            toMatchCdkSnapshot(): R;
-        }
-    }
-}
-export declare const toMatchCdkSnapshot: jest.CustomMatcher;
