94c38bba3956b8bd6495cbf1fb270cb5dcda8fe8
exoego <ytatsuno.jp@gmail.com>
2024-02-23 13:25:07 +0900

    Mask current version in all keys and values

--- src/index.ts
+++ src/index.ts
@@ -80,10 +80,17 @@ const maskCurrentVersionRefs = (tree: unknown): void => {
     }
   } else if (typeof tree === "object") {
     for (const [key, value] of Object.entries(tree)) {
-      if (key === "Ref" && typeof value === "string") {
-        const match = currentVersionRegex.exec(value);
-        if (match) {
-          tree[key] = `${match[1]}xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx`;
+      const keyMatch = currentVersionRegex.exec(key);
+      if (keyMatch) {
+        const newKey = `${keyMatch[1]}xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx`;
+        tree[newKey] = value;
+        delete tree[key];
+      }
+
+      if (typeof value === "string") {
+        const valueMatch = currentVersionRegex.exec(value);
+        if (valueMatch) {
+          tree[key] = `${valueMatch[1]}xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx`;
         }
       } else if (typeof value === "object") {
         maskCurrentVersionRefs(value as Record<string, unknown>);
@@ -137,15 +144,6 @@ const convertStack = (stack: Stack, options: Options = {}) => {
   }
 
   if (ignoreCurrentVersion && template.Resources) {
-    for (const [key, resource] of Object.entries(template.Resources)) {
-      const match = currentVersionRegex.exec(key);
-      if (match) {
-        const newKey = `${match[1]}xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx`;
-        template.Resources[newKey] = resource;
-        delete template.Resources[key];
-      }
-    }
-
     maskCurrentVersionRefs(template);
   }
 
